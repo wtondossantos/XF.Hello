ad6624b5d357390d94f4752a23f84b06d403b6ee
Wellington <wellington@wton.com.br>
2017-11-15 10:07:44 -0200

    Add project files.

---
+++ XF.Hello/XF.Hello/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Hello
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfiguracao_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
